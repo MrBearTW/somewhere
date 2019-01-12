2b3b2ec05fddf7274385dbdebbda7f39c42df6c6
MrBearTW <jamieperng@gmail.com>
2019-01-12 23:32:50 +0800

    20190112

--- go-training/test/sync.go
+++ go-training/test/sync.go
@@ -1,7 +1,27 @@
-func main() {
-	do(1)
-	do(2)
-	do(3)
+package main
 
-	time.Sleep(2 * time.Second)
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func do(i int, wg *sync.WaitGroup) {
+	fmt.Printf("start job: %d\n", i)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("end job: %d\n", i)
+	wg.Done()
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go do(1, &wg)
+	go do(2, &wg)
+	go do(3, &wg)
+
+	wg.Wait()
+	fmt.Println("Done")
+
+	//time.Sleep(2 * time.Second)
 }
