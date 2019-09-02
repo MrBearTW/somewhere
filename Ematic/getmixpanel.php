1db2e26de77c906cdd96332ebf4c666d390d5d76
MrBearTW <jamieperng@gmail.com>
2019-09-02 19:37:49 +0800

    20190902

--- Ematic/getmixpanel.php
+++ Ematic/getmixpanel.php
@@ -1,31 +1,38 @@
 <?php
-// 日期的for迴圈還沒補上
-// php.ini memory_limit=200M 建議要調200M以上 預設的128太小
+// php.ini memory_limit=200M 建議要調300M以上 預設的太小
 // 把$mixpanel_api_secret 換成 api_key 應該就可以了
 require_once 'setkey.php';
 
-$ch = curl_init();
+for ($d = strtotime("2018-09-08"); $d <= strtotime("2018-09-09"); $d = strtotime("+1 day", $d)) {
 
-$mixpanelapisecret = $mixpanel_api_secret;
-$date = "2019-08-13";
-$fsts = "from_date=" . $date . "&to_date=" . $date;
+    $date = date("Y-m-d", $d);
 
-curl_setopt($ch, CURLOPT_URL, 'https://data.mixpanel.com/api/2.0/export/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fsts);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_USERPWD, $mixpanelapisecret . ':' . '');
+    $fsts = "from_date=" . $date . "&to_date=" . $date;
 
-$headers = array();
-$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+    $ch = curl_init();
+    // $mixpanelapisecret = $mixpanel_api_secret;
+
+    curl_setopt($ch, CURLOPT_URL, 'https://data.mixpanel.com/api/2.0/export/');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fsts);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_USERPWD, $mixpanel_api_secret . ':');
+
+    $headers = array();
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+echo memory_get_usage() . " - begin\n";
+    $filename = "$date.json";
+    file_put_contents($filename, $result);
+    curl_close($ch);
+echo memory_get_usage() . " - function end\n";
+    unset($result);
+echo memory_get_usage() . " - end\n\n";
+
 }
-
-$filename = "$date.json";
-file_put_contents($filename, $result);
-
-curl_close($ch);
--- book/modern-php.md
+++ book/modern-php.md
@@ -1,3 +1,10 @@
 範例程式https://github.com/codeguy/modern-php  
 
-Ch2 命名空間
+- Ch2 
+    - 命名空間
+    - 介面 都是車 但寶馬和福特還是會不同
+    - 特徵機制trait 兩個不同祖先但有共同功能
+    - DRY: Do not repeat youself
+    - 產生器 只能往前跌代 節省記憶體
+    - 閉包 = 匿名函式 PHP中視為相同
+        - 
